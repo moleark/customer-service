a04bb8300a93a84a4d84266f013e1d666d36ae0b
ligsh <system6@jkchemical.com>
2020-12-29 17:21:21 +0800

    ligsh, v1.1.3, 未填写基本信息的客户审核界面显示其id

--- src/tvs/webuser.tsx
+++ src/tvs/webuser.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 export const tvWebUser = (value: any) => {
-    let { firstName, salutation, organizationName, departmentName } = value;
+    let { id, firstName, salutation, organizationName, departmentName } = value;
+    let showName = firstName || id;
+    let showOranization = organizationName || '【未填写单位名称】';
     return <>
-        <div className="mr-3"><strong>{firstName}</strong> <span className="small">{salutation}</span></div>
-        <div className="small text-muted">{organizationName} &nbsp;{departmentName}</div>
+        <div className="mr-3"><strong>{showName}</strong> <span className="small">{salutation}</span></div>
+        <div className="small text-muted">{showOranization} &nbsp;{departmentName}</div>
     </>
 }
 
